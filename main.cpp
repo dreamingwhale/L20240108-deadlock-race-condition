9717aeabf27c23fa5dc6cb6b2237cb085edb9c18
dreamingwhale <dreamingskywhale@gmail.com>
2024-01-22 15:53:33 +0900

    커널섹션(mutex)

--- main.cpp
+++ main.cpp
@@ -2,25 +2,32 @@
 #include <windows.h>
 #include <iostream>
 
+#pragma comment(lib,"winmm")
+
 using namespace std;
 
 int money = 0;
 int cash = -100;
 
 //임계영역 : 함수를 실행하는 동안 다른 스레드 실행하지마!
-CRITICAL_SECTION MoneySection;
-CRITICAL_SECTION MoneySection2;
+//CRITICAL_SECTION MoneySection;
+//CRITICAL_SECTION MoneySection2;
+
+DWORD StartTime = timeGetTime();	//프로그램이 실행되고 흘러간 시간
+
+HANDLE MoneyMutex;//스레드가 시그널 상태인지를 설정함.
+HANDLE ThreadHandle[2];
 
 unsigned Increase(void* Arg)
 {
 	for (int i = 0; i < 1000000; i++)
 	{
-		EnterCriticalSection(&MoneySection);	//임계영역 설정
-		EnterCriticalSection(&MoneySection2);
+		//시그널 상태가 될때까지 기다린다.
+		WaitForSingleObject(MoneyMutex, INFINITE);	//시그널 상태를 on
 		money++;
 		cash++;
-		LeaveCriticalSection(&MoneySection2);
-		LeaveCriticalSection(&MoneySection);
+		ReleaseMutex(MoneyMutex);					//시그널 상태를 off
+
 	}
 
 
@@ -30,12 +37,13 @@ unsigned Decrease(void* Arg)
 {
 	for (int i = 0; i < 1000000; i++)
 	{
-		EnterCriticalSection(&MoneySection);
-		EnterCriticalSection(&MoneySection2);	//임계영역 설정
+
+		WaitForSingleObject(MoneyMutex, INFINITE);
 		cash--;
 		money--;
-		LeaveCriticalSection(&MoneySection2);
-		LeaveCriticalSection(&MoneySection);
+		Sleep(0);	//이렇게 하면 무조건 문맥교환이 발생한다.
+		ReleaseMutex(MoneyMutex);
+
 	}
 
 	return 0;
@@ -45,33 +53,44 @@ unsigned Decrease(void* Arg)
 //main thread
 int main()
 {
-	InitializeCriticalSection(&MoneySection);	//임계영역 초기화
-	InitializeCriticalSection(&MoneySection2);
-	DWORD StartTime = timeGetTime();
-	HANDLE ThreadHandle1 = (HANDLE)_beginthreadex(
+	//Thread Pooling, IOCP
+	//Kernel Mode Lock object
+	MoneyMutex = CreateMutex(nullptr, FALSE, nullptr);
+
+	ThreadHandle[0] = (HANDLE)_beginthreadex(
 		nullptr
 		, 0
 		, Increase
 		, nullptr
-		, 0
+		, CREATE_SUSPENDED			//스레드 실행을 멈춰
 		, nullptr
 	);
 
-	HANDLE ThreadHandle2 = (HANDLE)_beginthreadex(
+	ThreadHandle[1] = (HANDLE)_beginthreadex(
 		nullptr
 		, 0
 		, Decrease
 		, nullptr
-		, 0
+		, CREATE_SUSPENDED
 		, nullptr
 	);
 
-	//메인스레드가 끝나면 그 안의 스레드가 실행되지 않고 그대로 끝나버린다. 따라서 내부의 스레드가 끝나고 난 후에 메인 스레드를 끝내도록 해야한다.
-	//위의 문제 때문에 race condition이라는 문제가 생겨버린다. 같은 메모리에 접근한 경우 한 스레드를 돌리다 다른 스레드를 실행, 그후 다시 기존 스레드를 실행, 이를 반복하면서 실제 원하던 값과 다른 값을 return한다.
-	Sleep(1000);
+	ResumeThread(ThreadHandle[0]);	//스레드 다시 실행해
+	ResumeThread(ThreadHandle[1]);
 
-	DeleteCriticalSection(&MoneySection);	//임계영역 해제
+	//SuspendThread(ThreadHandle[0]);	//스레드 멈춰
+	//SuspendThread(ThreadHandle[1]);
 
+	WaitForMultipleObjects(2, ThreadHandle, TRUE, INFINITE);
+
+	cout << "Process Time : " << timeGetTime() - StartTime<<endl;
+
+	//WaitForSingleObject(ThreadHandle, INFINITE);//스레드가 끝나길 기다려라
+	//WaitForSingleObject(ThreadHandle2, INFINITE);
+
+	CloseHandle(MoneyMutex);
+	CloseHandle(ThreadHandle[0]);
+	CloseHandle(ThreadHandle[1]);
 
 	cout << money << endl;
 	cout << cash << endl;
