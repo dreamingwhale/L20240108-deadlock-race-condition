5bc50cda825fed67b6772d369c6954622d19a909
dreamingwhale <dreamingskywhale@gmail.com>
2024-01-22 17:21:58 +0900

    tcp client복습

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <WinSock2.h>
+#include <iostream>
 
 #pragma comment(lib,"ws2_32")
 
@@ -8,41 +9,22 @@ int main()
 	WSAData wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	SOCKET ListenSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	struct sockaddr_in ListenSockAddr;
-	memset(&ListenSockAddr, 0, sizeof(ListenSockAddr));	//메모리를 비워라.
-	ListenSockAddr.sin_family = AF_INET;
-	ListenSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");	//INADDR_ANY 다 받아라
-	ListenSockAddr.sin_port = htons(10880);
-	bind(ListenSocket, (struct sockaddr*)&ListenSockAddr, sizeof(ListenSockAddr));
+	SOCKADDR_IN ServerSockAddr;
+	memset(&ServerSockAddr, 0, sizeof(ServerSockAddr));
+	ServerSockAddr.sin_family = AF_INET;
+	ServerSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ServerSockAddr.sin_port = htons(10880);
 
-	listen(ListenSocket, 5);
+	SOCKET ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	struct sockaddr_in ClientSockAddr;
-	memset(&ClientSockAddr, 0, sizeof(ClientSockAddr));	//메모리를 비워라.
-	int ClientSockAddrLength = sizeof(ClientSockAddr);
-	SOCKET ClientSocket = accept(ListenSocket, (struct sockaddr*)&ClientSockAddr, &ClientSockAddrLength);
+	connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
 
-	char Buffer[1024] = { 0, };
-	int RecvLength = recv(ClientSocket, Buffer, 1024, 0);
-	if (RecvLength == 0)
-	{
-		//정상종료
-	}
-	else if (RecvLength < 0)
-	{
-		//error
-
-	}
-	else
-	{
-		int SendLength = send(ClientSocket, Buffer, RecvLength, 0);//음수면 끊긴것, 0도 끊긴것, 2면 남아있는것
-	}
-
-
-	closesocket(ClientSocket);
-	closesocket(ListenSocket);
+	char Buffer[1024] = "Hello World";
+	int SerndLength = send(ServerSocket, Buffer, (int)strlen(Buffer), 0);
+	char Message[1024] = { 0, };
+	recv(ServerSocket, Message, sizeof(Message), 0);
+	std::cout << Message << std::endl;
+	
 
 	WSACleanup();
-	return 0;
 }
