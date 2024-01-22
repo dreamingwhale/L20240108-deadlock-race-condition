9cc27ff08362acf562f96df1f4a2ad42a6770e4c
dreamingwhale <dreamingskywhale@gmail.com>
2024-01-22 17:13:08 +0900

    tcp 통신 복습

--- main.cpp
+++ main.cpp
@@ -1,18 +1,48 @@
-#include <process.h>
-#include <windows.h>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WinSock2.h>
+
+#pragma comment(lib,"ws2_32")
 
 int main()
-{												//유니코드 프로그램을 helloworld라는 이름으로 만들겠다. 같은 이름으로 만들면 message를 던져라. 
-	HANDLE OnceMutex = CreateMutex(NULL, FALSE, L"HelloWorld");
+{
+	WSAData wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	if (GetLastError() == ERROR_ALREADY_EXISTS)
+	SOCKET ListenSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	struct sockaddr_in ListenSockAddr;
+	memset(&ListenSockAddr, 0, sizeof(ListenSockAddr));	//메모리를 비워라.
+	ListenSockAddr.sin_family = AF_INET;
+	ListenSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");	//INADDR_ANY 다 받아라
+	ListenSockAddr.sin_port = htons(10880);
+	bind(ListenSocket, (struct sockaddr*)&ListenSockAddr, sizeof(ListenSockAddr));
+
+	listen(ListenSocket, 5);
+
+	struct sockaddr_in ClientSockAddr;
+	memset(&ClientSockAddr, 0, sizeof(ClientSockAddr));	//메모리를 비워라.
+	int ClientSockAddrLength = sizeof(ClientSockAddr);
+	SOCKET ClientSocket = accept(ListenSocket, (struct sockaddr*)&ClientSockAddr, &ClientSockAddrLength);
+
+	char Buffer[1024] = { 0, };
+	int RecvLength = recv(ClientSocket, Buffer, 1024, 0);
+	if (RecvLength == 0)
 	{
-		MessageBox(nullptr, L"한번만 실행해라.", L"경고", MB_OK);
-		return 0;
+		//정상종료
 	}
-	while (true);
+	else if (RecvLength < 0)
+	{
+		//error
 
-	CloseHandle(OnceMutex);
-	
+	}
+	else
+	{
+		int SendLength = send(ClientSocket, Buffer, RecvLength, 0);//음수면 끊긴것, 0도 끊긴것, 2면 남아있는것
+	}
+
+
+	closesocket(ClientSocket);
+	closesocket(ListenSocket);
+
+	WSACleanup();
 	return 0;
 }
